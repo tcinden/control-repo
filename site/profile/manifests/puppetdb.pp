0ccddbcbf04f40607bbe61b9b7ebdcf58af34953
CookieMonster <me@me.com>
2021-03-19 00:45:51 -0700

    more config for puppetdb

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -1,4 +1,12 @@
 class profile::puppetdb {
-  class { 'puppetdb': }
-  class { 'puppetdb::master::config': }
+  class { 'puppetdb': 
+    open_listen_port => true,
+  }
+  class { 'puppetdb::master::config':
+    manage_storeconfigs => true,
+    manage_config       => true,
+    restart_puppet      => true,
+    enable_reports      => true,
+    enable_storeconfigs => true,
+  }
 }
