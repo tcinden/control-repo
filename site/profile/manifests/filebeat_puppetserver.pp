506a8ea846cdbdee13e4af6270bed6a9fdaef5fa
CookieMonster <me@me.com>
2021-03-19 12:45:46 -0700

    updated filebeat puppetserer profile with typos single quote and alignment fixes

--- site/profile/manifests/filebeat_puppetserver.pp
+++ site/profile/manifests/filebeat_puppetserver.pp
@@ -1,13 +1,13 @@
 class profile::filebeat_puppetserver {
   class{'elk::filebeat':
-    prospectors => [ {
-      "type" => "log",
-      "paths" => [
-        "/var/log/puppetlabs/puppetserver/puppetserver.log.json",
-        "/var/log/puppetlabs/puppetserver/puppetserver-access.log.json",
+    prospectors   => [{
+      'type'  => 'log',
+      'paths' => [
+        '/var/log/puppetlabs/puppetserver/puppetserver.log.json',
+        '/var/log/puppetlabs/puppetserver/puppetserver-access.log.json',
       ],
-      "json_keys_under_root" => true
-    } ],
+      'json_keys_under_root' => true
+    }],
   logstash_server => 'elk.local',
   }
 }
