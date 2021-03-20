b75b13793e018a0f3c8542fc42b9906ef60d17d0
CookieMonster <me@me.com>
2021-03-19 22:23:09 -0700

    puppetdb changes

--- site/profile/manifests/puppetdb.pp
+++ site/profile/manifests/puppetdb.pp
@@ -1,12 +1,19 @@
 class profile::puppetdb {
-  class { 'puppetdb': 
-    open_listen_port => true,
+  class { 'puppetdb':
+    open_listen_port      => true,
+    open_ssl_listen_port  => true,
+    manage_firewall       => true,
+    automatic_dlo_cleanup => true,
+    dlo_max_age           => 10,
   }
   class { 'puppetdb::master::config':
-    manage_storeconfigs => true,
-    manage_config       => true,
-    restart_puppet      => true,
-    enable_reports      => true,
-    enable_storeconfigs => true,
+    puppetdb_soft_write_failure    => true,
+    manage_storeconfigs            => true,
+    manage_report_processor        => true,
+    create_puppet_service_resource => true,
+    manage_config                  => true,
+    restart_puppet                 => true,
+    enable_reports                 => true,
+    enable_storeconfigs            => true,
   }
 }
